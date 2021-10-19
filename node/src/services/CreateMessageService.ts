4c3bf9f732f8804c5cb913dda799ffec1453e5d1
gabrielalimact <lgcl@ic.ufal.br>
2021-10-19 20:50:27 -0300

    websocket;  serverHttp

--- node/src/services/CreateMessageService.ts
+++ node/src/services/CreateMessageService.ts
@@ -1,5 +1,5 @@
 import prismaClient from "../prisma/index" 
-
+import { io } from "../app"
 class CreateMessageService {
     async execute(text: string, user_id: string) {
         const message = await prismaClient.message.create({
@@ -12,6 +12,17 @@ class CreateMessageService {
             }
         });
         
+        const infoWS = {
+            text: message.text,
+            user_id: message.user_id,
+            crete_at: message.create_at,
+            user: {
+                name: message.user.name,
+                avatar_url: message.user.avatar_url
+            }
+        };
+
+        io.emit("new_message", infoWS);
         return message;
     }
 }
