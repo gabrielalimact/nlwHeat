e1c5aaa4df901d43265b109f86b2995c7c675b18
gabrielalimact <lgcl@ic.ufal.br>
2021-10-22 09:37:57 -0300

    github auth

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,12 +1,16 @@
+import { useContext } from 'react'
 import { VscGithubInverted } from 'react-icons/vsc'
+import { AuthContext } from '../../contexts/auth'
+
 import styles from './styles.module.scss'
 
-
 export function LoginBox(){
+    const { signInUrl , user } = useContext(AuthContext)
+    console.log(user)
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.signWithGitHub}>
+            <a href={signInUrl} className={styles.signWithGitHub}>
                 <VscGithubInverted size="24"/> Entrar com GitHub
             </a>
         </div>
---
+++ web/src/contexts/auth.tsx
@@ -0,0 +1,68 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+type User = {
+    id: string;
+    name: string;
+    login: string;
+    avatar_url: string;
+}
+
+type AuthContextData = {
+    user: User | null;
+    signInUrl: string;
+}
+
+type AuthResponse = {
+    token: string;
+    user: {
+        id: string;
+        avatar_url: string;
+        name: string;
+        login: string;
+    }
+}
+
+type AuthProvider = {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+
+export function AuthProvider(props: AuthProvider) {
+    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=7c12988f959f438a1204`;
+    const [user, setUser] = useState<User | null>(null);
+
+
+    async function signIn (githubCode : string) {
+        const response = await api.post<AuthResponse>('authenticate', {
+            code: githubCode,
+        });
+
+        const { token, user } = response.data;
+
+        localStorage.setItem('@dowhile:token', token)
+        setUser(user)
+
+    }
+
+    useEffect(() => {
+        const url = window.location.href;
+        const codeGithub = url.includes('?code=')
+
+        if(codeGithub) {
+            const [urlWithouCode, githubCode] = url.split('?code=')
+
+            window.history.pushState({}, '', urlWithouCode)
+
+            signIn(githubCode)
+        }
+        
+    }, [])
+    return (
+        <AuthContext.Provider value={{ signInUrl, user}}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
