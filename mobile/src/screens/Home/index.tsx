c744c57a6b2bf542484aaeae0b2726ddbdac704a
gabrielalimact <lgcl@ic.ufal.br>
2021-10-24 15:54:56 -0300

    mobile template

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { 
+    View,
+    Text,
+    Image
+} from 'react-native';
+import { styles } from './styles'
+
+export function Home() {
+    
+    return (
+        <View style={styles.container}>
+        <Text>Olá, React Native</Text>
+        </View>
+    )
+
+}
